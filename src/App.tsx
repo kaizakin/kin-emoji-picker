55808eb53128193f40d8bbe5cd0b2449d916cc6d
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-10-25 17:35:13 +0530

    integrate clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
+import { writeText } from '@tauri-apps/plugin-clipboard-manager';
 
 function App() {
 
+  async function handleSelect(emoji: any){
+    await writeText(emoji.native);
+  }
+
   return (
     <div
-      className="overflow-auto resize-x min-w-[250px] w-[400px]"
+      className="overflow-auto"
       style={{ maxWidth: "100%" }}
     >
       <Picker
         data={data}
-        onEmojiSelect={console.log}
+        onEmojiSelect={handleSelect}
         previewPosition="none"
         searchPosition="sticky"
         theme="light"
         skinTonePosition="search"
         autoFocus={true}
-        emojiButtonRadius="6px"
-        emojiButtonColors={[
-          "rgba(155,223,88,.7)",
-          "rgba(149,211,254,.7)",
-          "rgba(247,233,34,.7)",
-          "rgba(238,166,252,.7)",
-          "rgba(255,213,143,.7)",
-          "rgba(211,209,255,.7)",
-        ]}
-        // dynamicWidth={true}
       />
     </div>
   );
