c0f47ad96034e2e013d41e4defc2de16d3c25727
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-10-25 18:39:11 +0530

    v1 ✅

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 import { writeText } from '@tauri-apps/plugin-clipboard-manager';
+import { Toaster, toast } from "sonner";
+
 
 function App() {
 
   async function handleSelect(emoji: any){
     await writeText(emoji.native);
+    toast.success(`Copied ${emoji.native} to clipboard!`);
   }
 
   return (
     <div
-      className="overflow-auto"
+      className="overflow-hidden overflow-y-hidden"
       style={{ maxWidth: "100%" }}
     >
+      <Toaster position="top-center" richColors theme='dark' duration={700} visibleToasts={1} closeButton />
       <Picker
         data={data}
         onEmojiSelect={handleSelect}
